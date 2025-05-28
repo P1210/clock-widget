bec8525f2d49c860d8eec990bccb53b2b535cac2
P1210 <gpranjal1210@gmail.com>
2025-05-28 21:03:40 +0530

    Initial project commit.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+// import "./bg.css";
+// import "./timer.css";
+// import Timer from "./Timer";
+
+function App() {
+  return (
+    <>
+      Clock Widget App
+      {/* <Timer /> */}
+    </>
+  );
+}
+
+export default App;
