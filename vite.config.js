b812a5b7b7bbc4192e21f34c8c781ee021ffdab0
P1210 <gpranjal1210@gmail.com>
2025-06-10 00:23:06 +0530

    Add github deployment configs.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/clock-widget/",
   plugins: [react()],
   server: {
     port: 5174,
